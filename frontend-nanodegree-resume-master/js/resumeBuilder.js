f6bc463fea2800610e6d4274a2dc940db835bc7e
Blenderhead69 <janus.zylka@gmx.de>
2015-12-13 17:13:42 +0100

    clean up resumeBuilder.js as it is recommended

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -6,47 +6,132 @@
 //funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 //$("#main").append(funThoughts);
 
-var formattedRole = HTMLheaderRole.replace("%data%", "Front End Web Developer");
-$("#header").prepend(formattedRole);
-var formattedName = HTMLheaderName.replace("%data%", "Janusz Eisse");
-$("#header").prepend(formattedName);
+//var formattedRole = HTMLheaderRole.replace("%data%", "Front End Web Developer");
+//$("#header").prepend(formattedRole);
+//var formattedName = HTMLheaderName.replace("%data%", "Janusz Eisse");
+//$("#header").prepend(formattedName);
 
-var bio = {
+
+//var work = {};
+//work.position = "Mediengestalter Digital und Print";
+//work.employer = "Agentur Bemberg GmbH";
+//work.years = "3 Years";
+//work.city = "Wuppertal";
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "Agentur Bemberg GmbH",
+		"title" : "Mediengestalter Digital und Print",
+		"location" : "Wuppertal, Germany",
+		"dates" : "2012 - ",
+		"description" : "Design, Developing, Final Artwork, Adobe Creative Suite, TYPO3, Git, PHPStorm: HTML, CSS, LESS, JavaScript"
+	},
+	{
+		"employer" : "NBC Europe",
+		"title" : "Internship for Television Broadcaster (Production)",
+		"location" : "Duesseldorf, Germany",
+		"dates" : "November 2004 - Mai 2005",
+		"description" : "Technical Support during a daily five hours live show"
+	}
+]
+};
+
+var projects = {
+	"projects" : [
+	{
+	"title" : "Fun In The Sun",
+	"dates" : "Summer of '69",
+	"description" : "Series of photographs during Trip through Europe",
+	"images" : ["/firstjpgurl.jpg", "/secondjpgurl.jpg", "/ohwowevenapng.png"]
+	},
+	{
+	"title" : "Friends Will Be Friends",
+	"dates" : "1990 - 2012",
+	"description" : "Playin' the drums at any kind of jams with my friends",
+	"images" : ["/firstjpgurl.jpg", "/secondjpgurl.jpg", "/ohwowevenapng.png"]
+	}
+]
+};
+
+ var bio = {
 	"name": "Janusz Zylka",
-	"role": "Web Developer",
+	"role": "Mediengestalter Digital and Print",
 	"contact": {
+		"mobile": "+49 176 341 222 333",
 		"email": "j.zylka@agentur-bemberg.de",
-		"phone": "+49 176 111 222 333",
-		"location": "Wuppertal/Germany"
+		"github" : "Blenderhead69",
+		"twitter" : "",
+		"location": "Wuppertal, Germany"
 	},
 	"picture": "images/me.jpg",
 	"message": "Hi. It's me.",
 	"skills": ["Jumping", "Turning Around", "Whistling (w & w/o fingers)", "Making the Air smell"]
 };
-var work = {};
-work.position = "Mediengestalter Digital und Print";
-work.employer = "Agentur Bemberg GmbH";
-work.years = "3 Years";
-work.city = "Wuppertal";
 
-var education = {};
-education["school"] = "FH Duesseldorf";
-education["years"] = "2 Years";
-education["city"] = "Duesseldorf";
+var education = {
+"schools" : [
+	{
+	"name" : "Carl-Duisberg-Gymnasium",
+	"location" : "Wuppertal, Germany",
+	"dates" : "1990 - 2003",
+	"majors" : "Mathematics, Geographie",
+	"graduation" : "A level, High School Diploma"
+	},
+	{
+	"name" : "RWTH Aachen, Geographie B.Sc.",
+	"location" : "Aachen, Germany",
+	"dates" : "2005 - 2007",
+	"degree" : "none"
+	},
+	{
+	"name" : "Berufskolleg am Kothen",
+	"location" : "Wuppertal, Germany",
+	"Dates" : "2007 - 2009",
+	"graduation" : "Staatlich geprüfter gestaltungstechnischer Assistent"
+	},
+	{
+	"name" : "FH Duesseldorf Media-Techniqe B.Eng.",
+	"location" : "Duesseldorf, Germany",
+	"Dates" : "2009 - 2012",
+	"graduation" : "none"
+	},
+	{
+	"name" : "Mediengestalter Digital and Print",
+	"location" : "Solingen, Germany",
+	"Dates" : "2013 - 2015",
+	"graduation" : "Mediengestalter/-in Digital und Print (IHK)"
+	}
 
+],
+"onlineCourses" : [
+	{
+	"name" : "Front End Nano Developer Programm",
+	"organization" : "udacity",
+	"graduation" : " ... ",
+	"url" : "www.udacity.com"
+	},
+	{
+	"name" : ["Make a Website", "HTML & CSS"],
+	"organization" : "codecademy",
+	"graduation" : "",
+	"url" : "www.codecademy.com"
+	}
+]
+};
 
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.contact.email);
-$("#main").append(bio.contact.phone);
-$("#main").append(bio.contact.location);
-$("#main").append(bio.picture);
-$("#main").append(bio.message);
-$("#main").append(bio.skills);
-$("#main").append(work.position);
-$("#main").append(work.employer);
-$("#main").append(work.years);
-$("#main").append(work.city);
-$("#main").append(education["school"]);
-$("#main").append(education["years"]);
-$("#main").append(education["city"]);
+//$("#main").append(bio.name);
+//$("#main").append(bio.role);
+//$("#main").append(bio.contact.email);
+//$("#main").append(bio.contact.phone);
+//$("#main").append(bio.contact.location);
+//$("#main").append(bio.picture);
+//$("#main").append(bio.message);
+//$("#main").append(bio.skills);
+//$("#main").append(work.position);
+//$("#main").append(work.employer);
+//$("#main").append(work.years);
+//$("#main").append(work.city);
+//$("#main").append(education["school"]);
+//$("#main").append(education["years"]);
+//$("#main").append(education["city"]);
